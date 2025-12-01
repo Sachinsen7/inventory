31acaf5a364fd0ab0be81e261350d4c90a5d77c8
Sachinsen7 <sachinsen1920@gmail.com>
2025-12-01 16:08:52 +0530

    style(Users): Fix spacing in async function declaration
    - Add space before parentheses in async function pre-hook for consistency
    - Improve code formatting to match project style guidelines

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // Password hashing before saving user
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
